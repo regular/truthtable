e10460b2eab96fa43b12b321586118c566a8f0c9
Jan Bölsche <regular.gonzales@googlemail.com>
2015-06-14 23:09:39 +0200

    Add table headers

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ var styles = require('./styles.styl');
 var insertCSS = require('insert-css');
 insertCSS(styles);
 
-function truthtable(el, rows, options) {
+function truthtable(el, data, options) {
     var doc = el.ownerDocument;
     var symbols = '01';
 
-    function appendFields(tr, fields) {
+    function appendFields(tr, fields, options) {
+        options = options || {};
+        var contentFilter = options.contentFilter || function(x) {return x;};
+
         for (var i=0, l=fields.length; i<l; ++i) {
             var td = doc.createElement('div');
             td.classList.add('cell');
-            td.innerHTML = fields[i];
+            td.innerHTML = contentFilter(fields[i]);
             var symbol = fields[i];
             if (symbols.indexOf(symbol)>0) {
                 td.classList.add('on');
@@ -21,18 +24,18 @@ function truthtable(el, rows, options) {
         }
     }
 
-    function appendRow(table, row) {
+    function appendRow(table, row, options) {
         var tr = doc.createElement('div');
         tr.classList.add('row');
         
         var inputs = doc.createElement('div');
         inputs.classList.add('inputs');
-        appendFields(inputs, row.inputs);
+        appendFields(inputs, row.inputs, options);
         tr.appendChild(inputs);
 
         var outputs = doc.createElement('div');
         outputs.classList.add('outputs');
-        appendFields(outputs, row.outputs);
+        appendFields(outputs, row.outputs, options);
         tr.appendChild(outputs);
 
         table.appendChild(tr);
@@ -40,8 +43,13 @@ function truthtable(el, rows, options) {
 
     var table = doc.createElement('div');
     table.classList.add('truthtable');
-    for (var i=0, l=rows.length; i<l; ++i) {
-        appendRow(table, rows[i]);
+    appendRow(table, data.header, {contentFilter: function(s) {
+        return s.replace(/([0-9]*)$/g, function(a,b,c) {
+            return "<sub>" + b + "</sub>";
+        });
+    }});
+    for (var i=0, l=data.rows.length; i<l; ++i) {
+        appendRow(table, data.rows[i]);
     }
     el.appendChild(table);
 }
@@ -52,5 +60,10 @@ var rows = [
     {inputs: [0,0,1,0], outputs: [1,1]}
 ];
 
-truthtable(document.body, rows);
+var table = {
+    header: {inputs: 'a3 a2 a1 a0'.split(' '), outputs: 'y1 y0'.split(' ')},
+    rows: rows
+};
+
+truthtable(document.body, table);
 
